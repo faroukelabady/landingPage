5a2a6187f674ae8c9bdc5bba1e0ba936e8fcd9f0
farouk <faroukelabady@hotmail.com>
2020-10-17 01:14:28 +0200

    feat: add scroll to top button

--- js/app.js
+++ js/app.js
@@ -18,8 +18,9 @@
  *
  */
 
-let previousSelection = document.getElementById("section1");
-let isScrolling;
+let previousSelection = document.getElementById('section1');
+let scrollTimeout;
+let scrollToTopTimeout;
 /**
  * End Global Variables
  * Start Helper Functions
@@ -27,17 +28,17 @@ let isScrolling;
  */
 
 function createAnchorElement(id, text) {
-  const anchor = document.createElement("a");
-  anchor.classList.add("no__style");
+  const anchor = document.createElement('a');
+  anchor.classList.add('no__style');
   anchor.href = `#${id}`;
   anchor.textContent = text;
   return anchor;
 }
 
 function createListItemElement(anchorElement) {
-  const listItem = document.createElement("li");
+  const listItem = document.createElement('li');
   listItem.appendChild(anchorElement);
-  listItem.classList.add("menu__link");
+  listItem.classList.add('menu__link');
   return listItem;
 }
 
@@ -49,23 +50,44 @@ function scrollToSection(event) {
     `a[href="#${sectionElement.id}"]`
   );
   const previous = document.querySelector(`a[href="#${previousSelection.id}"]`);
-  if (previous.parentElement.classList.contains("selected")) {
-    previous.parentElement.classList.toggle("selected");
+  if (previous.parentElement.classList.contains('selected')) {
+    previous.parentElement.classList.toggle('selected');
   }
-
-  anchorElement.parentElement.classList.toggle("selected");
-
-  previousSelection.classList.toggle("active");
-  sectionElement.classList.toggle("active");
-  sectionElement.scrollIntoView({ behavior: "smooth", block: "end" });
+  anchorElement.parentElement.classList.toggle('selected');
+  previousSelection.classList.toggle('active');
+  sectionElement.classList.toggle('active');
+  sectionElement.scrollIntoView({ behavior: 'smooth', block: 'end' });
   previousSelection = sectionElement;
 }
 
 function hideNavBarOnIdle() {
   setTimeout(() => {
-    const navBar = document.getElementById("navbarMenu");
+    const navBar = document.getElementById('navbarMenu');
     navBar.hidden = true;
   }, 2000);
+}
+
+function isAtBottom(element) {
+  let bottom = (window.innerHeight + window.scrollY) >= document.body.offsetHeight; 
+  return bottom;
+}
+
+function showScrollToTopButton() {
+  const footer = document.getElementById('footer');
+  const scrollButton =  footer.querySelector('#scrollTop');
+   if(isAtBottom(footer)) {
+    scrollButton.classList.remove('hidden');
+   } else {
+    scrollButton.classList.add('hidden')
+   }
+}
+
+function scrollToTop() {
+  const scrollButton =  document.querySelector('#scrollTop');
+  scrollButton.addEventListener('click', () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  });
 }
 
 /**
@@ -75,33 +97,40 @@ function hideNavBarOnIdle() {
  */
 
 function showNavBarOnScroll() {
-  const body = document.getElementById("content");
-  window.addEventListener("scroll", () => {
-    const navBar = document.getElementById("navbarMenu");
-    window.clearTimeout(isScrolling);
+  const body = document.getElementById('content');
+  window.addEventListener('scroll', () => {
+    const navBar = document.getElementById('navbarMenu');
+    window.clearTimeout(scrollTimeout);
+    window.clearTimeout(scrollToTopTimeout);
     if (navBar.hidden) {
       navBar.hidden = false;
     }
-    isScrolling = setTimeout(hideNavBarOnIdle, 50);
+    scrollTimeout = setTimeout(hideNavBarOnIdle, 50);
+    scrollToTopTimeout = setTimeout(showScrollToTopButton, 30);
   });
+}
+
+function ScrollToTop() {
+  const footer = document.getElementById('footer');
+ 
 }
 
 // build the nav
 function buildNavigationMenu() {
   const fragment = document.createDocumentFragment();
-  const sections = document.getElementsByTagName("section");
-  const navBarList = document.getElementById("navbar__list");
-  navBarList.addEventListener("click", scrollToSection);
+  const sections = document.getElementsByTagName('section');
+  const navBarList = document.getElementById('navbar__list');
+  navBarList.addEventListener('click', scrollToSection);
   for (let section of sections) {
     const anchor = createAnchorElement(
       section.id,
-      section.getAttribute("data-nav")
+      section.getAttribute('data-nav')
     );
     const listItem = createListItemElement(anchor);
     fragment.appendChild(listItem);
   }
 
-  let ul = document.getElementById("navbar__list");
+  let ul = document.getElementById('navbar__list');
   ul.appendChild(fragment);
   hideNavBarOnIdle();
 }
@@ -112,3 +141,4 @@ function buildNavigationMenu() {
  */
 buildNavigationMenu();
 showNavBarOnScroll();
+scrollToTop();
