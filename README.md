4adf226318830a7b2ab0a9a1fa0827099c346aad
farouk <faroukelabady@hotmail.com>
2020-10-16 18:20:17 +0200

    feat: dynamic navigation menu scrolling

---
+++ README.md
@@ -0,0 +1,11 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. adding some interactivity using javascript inside `ja/app.js`.
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
---
+++ js/app.js
@@ -0,0 +1,79 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+
+let previousSelection = document.getElementById("section1");
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+function createAnchorElement(id, text) {
+  const anchor = document.createElement("a");
+  anchor.classList.add('nostyle');
+  anchor.href = `#${id}`;
+  anchor.textContent = text;
+  return anchor;
+}
+
+function createListItemElement(anchorElement) {
+  const listItem = document.createElement("li");
+  listItem.appendChild(anchorElement);
+  listItem.classList.add("menu__link");
+  return listItem;
+}
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const sectionname = event.target.textContent;
+  const sectionElement = document.querySelector(`[data-nav="${sectionname}"]`);
+  previousSelection.classList.toggle("active");
+  sectionElement.classList.toggle("active");
+  sectionElement.scrollIntoView({ behavior: "smooth", block: "end" });
+  previousSelection = sectionElement;
+}
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+function buildNavigationMenu() {
+  const sections = document.getElementsByTagName("section");
+  const fragment = document.createDocumentFragment();
+  document.getElementById('navbar__list').addEventListener("click", scrollToSection);
+  for (let section of sections) {
+    const anchor = createAnchorElement(section.id, section.getAttribute("data-nav"));
+    const listItem = createListItemElement(anchor);
+    fragment.appendChild(listItem);
+  }
+
+  let ul = document.getElementById("navbar__list");
+  ul.appendChild(fragment);
+}
+
+/**
+ * End Main Functions
+ * 
+ */
+buildNavigationMenu();
