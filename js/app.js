da2cb621860eb597a17dc0203abfca7bf0456c3b
farouk <faroukelabady@hotmail.com>
2020-10-17 01:13:44 +0200

    feat: auto hide naviation menu

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@
  */
 
 let previousSelection = document.getElementById("section1");
-
+let isScrolling;
 /**
  * End Global Variables
  * Start Helper Functions
@@ -28,7 +28,7 @@ let previousSelection = document.getElementById("section1");
 
 function createAnchorElement(id, text) {
   const anchor = document.createElement("a");
-  anchor.classList.add('nostyle');
+  anchor.classList.add("no__style");
   anchor.href = `#${id}`;
   anchor.textContent = text;
   return anchor;
@@ -45,10 +45,27 @@ function scrollToSection(event) {
   event.preventDefault();
   const sectionname = event.target.textContent;
   const sectionElement = document.querySelector(`[data-nav="${sectionname}"]`);
+  const anchorElement = document.querySelector(
+    `a[href="#${sectionElement.id}"]`
+  );
+  const previous = document.querySelector(`a[href="#${previousSelection.id}"]`);
+  if (previous.parentElement.classList.contains("selected")) {
+    previous.parentElement.classList.toggle("selected");
+  }
+
+  anchorElement.parentElement.classList.toggle("selected");
+
   previousSelection.classList.toggle("active");
   sectionElement.classList.toggle("active");
   sectionElement.scrollIntoView({ behavior: "smooth", block: "end" });
   previousSelection = sectionElement;
+}
+
+function hideNavBarOnIdle() {
+  setTimeout(() => {
+    const navBar = document.getElementById("navbarMenu");
+    navBar.hidden = true;
+  }, 2000);
 }
 
 /**
@@ -57,23 +74,41 @@ function scrollToSection(event) {
  *
  */
 
+function showNavBarOnScroll() {
+  const body = document.getElementById("content");
+  window.addEventListener("scroll", () => {
+    const navBar = document.getElementById("navbarMenu");
+    window.clearTimeout(isScrolling);
+    if (navBar.hidden) {
+      navBar.hidden = false;
+    }
+    isScrolling = setTimeout(hideNavBarOnIdle, 50);
+  });
+}
+
 // build the nav
 function buildNavigationMenu() {
   const fragment = document.createDocumentFragment();
   const sections = document.getElementsByTagName("section");
-  document.getElementById('navbar__list').addEventListener("click", scrollToSection);
+  const navBarList = document.getElementById("navbar__list");
+  navBarList.addEventListener("click", scrollToSection);
   for (let section of sections) {
-    const anchor = createAnchorElement(section.id, section.getAttribute("data-nav"));
+    const anchor = createAnchorElement(
+      section.id,
+      section.getAttribute("data-nav")
+    );
     const listItem = createListItemElement(anchor);
     fragment.appendChild(listItem);
   }
 
   let ul = document.getElementById("navbar__list");
   ul.appendChild(fragment);
+  hideNavBarOnIdle();
 }
 
 /**
  * End Main Functions
- * 
+ *
  */
 buildNavigationMenu();
+showNavBarOnScroll();
